b93ec1680bbbf1c12b22163f7f4e5e664f7700b5
CCVDEMO2 <ccvdemo2@node1103.oscar.ccv.brown.edu>
2023-06-07 13:02:38 -0400

    adding file

---
+++ application-level/restore_from_checkpoint.py
@@ -0,0 +1,74 @@
+import os
+import tensorflow as tf
+from tensorflow import keras
+
+print(tf.version.VERSION)
+
+#####Get an example dataset - we'll use the MNIST dataset first 1000 examples:
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+
+train_labels = train_labels[:1000]
+test_labels = test_labels[:1000]
+
+train_images = train_images[:1000].reshape(-1, 28 * 28) / 255.0
+test_images = test_images[:1000].reshape(-1, 28 * 28) / 255.0
+
+####Define a simple sequential model:
+def create_model():
+  model = tf.keras.models.Sequential([
+    keras.layers.Dense(512, activation='relu', input_shape=(784,)),
+    keras.layers.Dropout(0.2),
+    keras.layers.Dense(10)
+  ])
+
+  model.compile(optimizer='adam',
+                loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+                metrics=[tf.metrics.SparseCategoricalAccuracy()])
+
+  return model
+
+# Create a basic model instance
+model = create_model()
+
+# Display the model's architecture
+model.summary()
+
+
+#####Checkpointing - Create a tf.keras.callbacks.ModelCheckpoint callback that saves weights only during training:
+
+checkpoint_path = "training_1/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+
+# Train the model with the new callback
+model.fit(train_images, 
+          train_labels,  
+          epochs=10,
+          validation_data=(test_images,test_labels),
+          callbacks=[cp_callback])  # Pass callback to training
+
+# This may generate warnings related to saving the state of the optimizer.
+# These warnings (and similar warnings throughout this notebook)
+# are in place to discourage outdated usage, and can be ignored.
+
+######rebuild a fresh, untrained model, and evaluate it on the test set (~10% accuracy):
+
+# Create a basic model instance
+model = create_model()
+
+# Evaluate the model
+loss, acc = model.evaluate(test_images,  test_labels, verbose=2)
+print("Untrained model, accuracy: {:5.2f}%".format(100*acc))
+
+##### Testing the Checkpoint - load the weights from the checkpoint and re-evaluate:
+
+# Loads the weights
+model.load_weights(checkpoint_path)
+
+# Re-evaluate the model
+loss,acc = model.evaluate(test_images,  test_labels, verbose=2)
+print("Restored model, accuracy: {:5.2f}%".format(100*acc))
